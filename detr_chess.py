f3516b8dce73bc96dd944bb26790389d789a4b7b
CYLi <taipingeric@gmail.com>
2022-06-18 11:29:36 +0800

    add config

---
+++ detr_chess.py
@@ -0,0 +1,63 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = ['downloads/detr_r50_8x2_150e_coco.py']
+
+# We also need to change the num_classes in head to match the dataset's annotation
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(
+            num_classes=13))
+)
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('bishop',
+    'black-bishop',
+    'black-king',
+    'black-knight',
+    'black-pawn',
+    'black-queen',
+    'black-rook',
+    'white-bishop',
+    'white-king',
+    'white-knight',
+    'white-pawn',
+    'white-queen',
+    'white-rook')
+data_root = 'data/chess/'
+
+data = dict(
+    train=dict(
+        img_prefix='data/chess/train/',
+        classes=classes,
+        ann_file='data/chess/train/_annotations.coco.json'),
+    val=dict(
+        img_prefix='data/chess/valid/',
+        classes=classes,
+        ann_file='data/chess/valid/_annotations.coco.json'),
+    test=dict(
+        img_prefix='data/chess/test/',
+        classes=classes,
+        ann_file='data/chess/test/_annotations.coco.json',
+        # pipeline=[
+        #     dict(type='LoadImageFromFile'),
+        #     dict(
+        #         type='MultiScaleFlipAug',
+        #         img_scale=(1333, 800),
+        #         flip=False,
+        #         transforms=[
+        #             dict(type='Resize', keep_ratio=True),
+        #             dict(type='RandomFlip'),
+        #             dict(
+        #                 type='Normalize',
+        #                 mean=[123.675, 116.28, 103.53],
+        #                 std=[58.395, 57.12, 57.375],
+        #                 to_rgb=True),
+        #             dict(type='Pad', size_divisor=32),
+        #             dict(type='ImageToTensor', keys=['img']),
+        #             dict(type='Collect', keys=['img'])
+        #         ])
+        # ]
+    )
+)
+
+runner = dict(type='EpochBasedRunner', max_epochs=12)
+checkpoint_config = dict(interval=3)
