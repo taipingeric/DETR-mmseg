92e1bb2d16aa0648dfb7e7d9e391ce9fb7ddfcc0
CYLi <taipingeric@gmail.com>
2022-06-18 11:33:38 +0800

    update config

--- detr_chess.py
+++ detr_chess.py
@@ -37,25 +37,6 @@ data = dict(
         img_prefix='data/chess/test/',
         classes=classes,
         ann_file='data/chess/test/_annotations.coco.json',
-        # pipeline=[
-        #     dict(type='LoadImageFromFile'),
-        #     dict(
-        #         type='MultiScaleFlipAug',
-        #         img_scale=(1333, 800),
-        #         flip=False,
-        #         transforms=[
-        #             dict(type='Resize', keep_ratio=True),
-        #             dict(type='RandomFlip'),
-        #             dict(
-        #                 type='Normalize',
-        #                 mean=[123.675, 116.28, 103.53],
-        #                 std=[58.395, 57.12, 57.375],
-        #                 to_rgb=True),
-        #             dict(type='Pad', size_divisor=32),
-        #             dict(type='ImageToTensor', keys=['img']),
-        #             dict(type='Collect', keys=['img'])
-        #         ])
-        # ]
     )
 )
 
